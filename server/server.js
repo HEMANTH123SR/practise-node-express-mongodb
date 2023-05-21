3ea02eb28378ad80857327ae344bfb98755c44a2
hemanth <applicationcomputer257@gmail.com>
2023-05-21 12:18:23 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -15,66 +15,78 @@ const data=[{
 
 // rest api
 
-app.get('/',(req,res)=>{
-res.status(200).json(
-    {
-        status:"success",
-        data:data
-    }
-)
-})
+//function
+
+const getAllData = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: data,
+  });
+};
 
 
-app.get('/:id',(req,res)=>{
-    const id=req.params.id*1
-    if(!id>data.length){
-res.status(200).json(
-    {
-        status:"success",
-        data:data[id]
-    }
-)
-    }
-    else{
-        res.status(400).json(
-            {
-                status:"unsuccess",
-                data:"sorry we could not process the request"
-            }
-        )
-    }
-    
-})
+const postData = (req, res) => {
+  const d = req.body;
+  if (d) {
+    data.push(d);
+    res.status(201).json({
+      status: "success",
+      data: data[data.length - 1],
+    });
+  } else {
+    res.status(400).json({
+      status: "unsuccess",
+      data: "sorry the server could not process they request",
+    });
+  }
+};
 
-app.post('/',(req,res)=>{
-    const d=req.body
-    if(d){
-        data.push(d)
-        res.status(201).json(
-            {
-                status:"success",
-                data:data[data.length-1]
-            }
-        )
-    }
-    else{
-        res.status(400).json(
-            {
-                status:"unsuccess",
-                data:"sorry the server could not process they request"
-            }
-        )
-    }
-})
+const getDataById = (req, res) => {
+  const id = req.params.id * 1;
+  if (id < data.length) {
+    res.status(200).json({
+      status: "success",
+      data: data[id],
+    });
+  } else {
+    res.status(400).json({
+      status: "unsuccess",
+      data: "sorry we could not process the request",
+    });
+  }
+};
+
+const deleteData = (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  if (id < data.length) {
+    delete data[id];
+    res.status(204);
+  } else {
+    res.status(400).json({
+      status: "success",
+      data: "sorry we could not process the request",
+    });
+  }
+};
+
+const patchData=(req,res)=>{
+
+}
 
 
+app
+.route('/')
+.get(getAllData)
+.post(postData)
 
-app.delete('/:id',(req,res)=>{
-    const id=req.params.id;
-    if(id>=data.length){
-        
-    }
-})
+app
+.route('/:id')
+.get(getDataById)
+.delete(deleteData)
+.patch(patchData)
+
+
 app.listen(port,()=>{
     console.log(`the server started listening request at port http://locahost:${port}`)
 })
